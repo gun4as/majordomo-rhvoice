64b35ffd3967512e608a859071f0e4197d5a1c17
BuildTools <unconfigured@null.spigotmc.org>
2020-02-10 21:31:39 +0200

    add lv language file

---
+++ languages/rhvoice_lv.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Default language file for RHVoice module
+ *
+ */
+$dictionary = array(
+    'ABOUT' => 'Par',
+    'SETTINGS' => 'Settings',
+    'HELP' => 'Palīdzība',
+    'VOICE' => 'Balss:',
+    'USE_SPD' => 'Izmantot Speech Dispatcher',
+    'USE_CACHE' => 'Izmantot kešošnu',
+    'RHVOICE_INSTALLATION' => 'RHVoice instalācija',
+    'VOICE_ALEKSANDR' => 'Aleksandrs Krieviski',
+    'VOICE_ELENA' => 'Elena Krieviski',
+    'VOICE_ANNA' => 'Anna Krieviski',
+    'VOICE_CLB' => 'Angļu sieviete',
+    'VOICE_SLT' => 'Angļu vīrietis',
+
+);
+
+foreach ($dictionary as $k => $v) {
+    if (!defined('LANG_' . $k)) {
+        define('LANG_' . $k, $v);
+    }
+}
+?>
